beaecf99f05f9b58960973f61fb5d48d85940a8e
Maryna Boholiubova <astoret2015@gmail.com>
2026-05-05 14:24:24 +0200

    react-movies

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import type { Movie } from "./types/movie";
+import { fetchMovies } from "./services/movieService";
+import SearchBar from "./components/SearchBar/SearchBar";
+import MovieGrid from "./components/MovieGrid/MovieGrid";
+import Loader from "./components/Loader/Loader";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import MovieModal from "./components/MovieModal/MovieModal";
+import toast from "react-hot-toast";
+import css from "./App.module.css";
+
+export default function App() {
+ 
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+  const [selectedMovie, setSelectedMovie] =
+    useState<Movie | null>(null);
+
+  const handleSearch = async (query: string) => {
+    try {
+      setMovies([]); 
+      setError(false);
+      setLoading(true);
+
+      const data = await fetchMovies(query);
+
+      if (!data.length) {
+        toast.error("No movies found for your request.");
+      }
+
+      setMovies(data);
+    } catch (err) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className={css.app}>
+      {}
+      <SearchBar onSubmit={handleSearch} />
+
+      {}
+      {loading && <Loader />}
+
+      {}
+      {error && <ErrorMessage />}
+
+      {}
+      {!loading && !error && (
+        <MovieGrid
+          movies={movies}
+          onSelect={setSelectedMovie}
+        />
+      )}
+
+      {}
+      {selectedMovie && (
+        <MovieModal
+          movie={selectedMovie}
+          onClose={() => setSelectedMovie(null)}
+        />
+      )}
+    </div>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Toaster } from "react-hot-toast";
+import "modern-normalize/modern-normalize.css";
+
+
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
+  <React.StrictMode>
+    <App />
+    <Toaster />
+  </React.StrictMode>
+);
+
