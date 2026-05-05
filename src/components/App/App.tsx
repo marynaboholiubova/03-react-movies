7aedf092c31759864907ac6f1e5304bc83a168d8
Maryna Boholiubova <astoret2015@gmail.com>
2026-05-05 14:49:40 +0200

    hw-react-movies

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import type { Movie } from "./types/movie";
-import { fetchMovies } from "./services/movieService";
-import SearchBar from "./components/SearchBar/SearchBar";
-import MovieGrid from "./components/MovieGrid/MovieGrid";
-import Loader from "./components/Loader/Loader";
-import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
-import MovieModal from "./components/MovieModal/MovieModal";
+import type { Movie } from "../../types/movie";
+import { fetchMovies } from "../../services/movieService";
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
 import toast from "react-hot-toast";
 import css from "./App.module.css";
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./components/App/App";
 import { Toaster } from "react-hot-toast";
 import "modern-normalize/modern-normalize.css";
 
